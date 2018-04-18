0442adecd30ad8141d56c96721c3091675587bf6
JesusT97 <elbauldelocio@gmail.com>
2018-04-18 15:37:41 -0500

    Prev and Next Buttons
    The Last and first image don't have button of prev and next respectively.

--- js/master.js
+++ js/master.js
@@ -1,42 +1,55 @@
-$(".element").click(function(){
+$(document).on('click','.element',function(){
   var selected=$(this);
   var prev=$(this).prev().find('img');
   var next=$(this).next().find('img');
   $('#screen').show();
-  var l=$(".element").length;
+  var l=$(".element").length-1;
   var p=$(".element").index(selected);
-  if (l > 1) {
-    if (p == 0){
-      $("#screen").html('<div class="full bt">&#11016</div><div class="close bt">&times</div><div class="image"></div><div class="nxt bt">&rarr;</div>');
-    }
-    else if (p == l) {
-      $("#screen").html('<div class="full bt">&#11016</div><div class="close bt">&times</div><div class="image"></div><div class="prev bt">&larr;</div>');
+  function buttons(){
+    if (l > 1) {
+      if (p == 0){
+        return '<div class="full bt">&#11016</div><div class="close bt">&times</div><div class="nxt bt">&rarr;</div>';
+      }
+      else if (p == l) {
+        return '<div class="full bt">&#11016</div><div class="close bt">&times</div><div class="prev bt">&larr;</div>';
+      }
+      else{
+        return '<div class="full bt">&#11016</div><div class="close bt">&times</div><div class="nxt bt">&rarr;</div><div class="prev bt">&larr;</div>';
+      }
     }
     else{
-      $("#screen").html('<div class="full bt">&#11016</div><div class="close bt">&times</div><div class="image"></div><div class="nxt bt">&rarr;</div><div class="prev bt">&larr;</div>');
+      return '<div class="full bt">&#11016</div><div class="close bt">&times</div>';
     }
   }
-  else{
-    $("#screen").html('<div class="full bt">&#11016</div><div class="close bt">&times</div><div class="image"></div>');
-  }
+  buttons();
+  var content=buttons();
+  $("#screen").html('<div class="image"></div>' + content);
   $(".image").html('<img src="'+ $('img', this).attr('src') +'">');
   $("body").css('overflow','hidden');
-  $("#screen .close").click(function(){
+  $(document).on('click','.close',function(){
     $("#screen").hide();
     $("body").css('overflow','auto');
   });
-  $(".prev").click(function(){
+  $(document).on('click','.prev',function(){
     selected=selected.prev();
-    $(".image").html('<img src="'+ prev.attr('src') +'">');
-    prev=selected.prev().find('img');
+    prev=selected.find('img');
+    var previmg='<img src="'+ prev.attr('src') +'">';
+    $(".image").html(previmg);
     p=$(".element").index(selected);
+    buttons();
+    content=buttons();
+    $("#screen").html('<div class="image">'+ previmg + '</div>' + content);
   });
-  $(".nxt").click(function(){
+  $(document).on('click','.nxt',function(){
     selected=selected.next();
-    $(".image").html('<img src="'+ next.attr('src') +'">');
-    next=selected.next().find('img');
+    next=selected.find('img');
+    var nxtimg='<img src="'+ next.attr('src') +'">';
+    $(".image").html(nxtimg);
     p=$(".element").index(selected);
+    buttons();
+    content=buttons();
+    $("#screen").html('<div class="image">'+ nxtimg + '</div>' + content);
   });
-  $(".full").click(function(){
+  $(document).on('click','.full',function(){
   });
 });
